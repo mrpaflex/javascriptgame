0b7349cc804a5784bca6b5eb3b57aaff4087b3f3
mrpaflex <ukosaviour21@gmail.com>
2023-07-18 18:19:43 +0100

    pick a color of your choice

---
+++ js/main.js
@@ -0,0 +1,40 @@
+document.querySelector('#play').addEventListener('click', runcheck);
+
+
+
+
+function randomd(){
+    let result = Math.random();
+    if (result < .33) {
+        return('red');
+        
+    }else if(result < .66){
+        return('pink');
+    }else{
+        return('yellow');
+    }
+
+}
+
+function runcheck(){
+    let userchoice = String(document.querySelector('#choice').value).toLowerCase();
+ 
+    let botchoice = randomd();
+    if ((userchoice === 'red' && botchoice === 'yellow') || (userchoice === 'pink' && botchoice === 'red') || (userchoice === 'yellow' && botchoice === 'pink') ) {
+        //console.log('win');
+        document.querySelector('#check').innerHTML = `Wou Win... Wongratulation!`;
+        //document.querySelector('#bott').innerText = result;
+        document.querySelector('body').style.backgroundColor = 'green';
+    }else if(userchoice === botchoice){
+       // console.log('tied');
+        document.querySelector('#check').innerHTML = `you tied... try again`
+        //document.querySelector('#bott').innerText = result;
+        document.querySelector('body').style.backgroundColor = 'yellow';
+    }else{
+        //console.log('lose');
+        document.querySelector('#check').innerHTML = `You lose!`
+        //document.querySelector('#bott').innerText = result;
+        document.querySelector('body').style.backgroundColor = 'red';
+    }
+}
+checkWin()
